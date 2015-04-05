478e22968af2e459b61a49127852dad8076208b4
arpisab <sabino.dargenio@gmail.com>
2015-04-05 18:10:09 +0100

    Stack

--- C++/Basis_01/Pointers.h
+++ C++/Basis_01/Pointers.h
@@ -20,3 +20,32 @@ int main(){
 
   return 0;
 }
+
+// case 2
+using namespace std;
+
+int main() {
+
+
+	int a = 5;
+
+	int *p;
+
+	p = &a;
+
+	printf("%i \n", p); // It prints a position in memory
+	printf("%d \n", p); // It prints a position in memory
+	printf("%p \n", p); // It prints p position in memory
+	printf("%d \n", a); // It prints a value
+	printf("%d \n", &a); // It prints a position in memory
+
+	printf("%d \n", *p); // It prints value at address - "dereferencing"
+
+	*p = 8;
+	printf("%d \n", *p);
+	printf("%d \n", a);
+
+	system("PAUSE");
+
+	return 0;
+}
