25e6407a268a5b152eaf6677d7fa2a9e81c07198
arpisab <sabino.dargenio@gmail.com>
2015-06-10 20:42:36 +0100

    stuff

--- C#/Variables/Variables/Program.cs
+++ C#/Variables/Variables/Program.cs
@@ -10,12 +10,40 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            int x;
-            int y;
+            //int x;
+            //int y;
 
-            x = 7;
-            y = x + 3;
-            Console.WriteLine(y);
+            //x = 7;
+            //y = x + 3;
+            //Console.WriteLine(y);
+
+            //string myFirstName;
+           // myFirstName = "Bob";
+
+
+
+            // -> string myFirstName = "Bob";
+
+            // var myFirstName = true;
+
+            int x = 7;
+            string y = "Bob";
+           
+            // string conversion -----------------------------
+            // -> string myFirstTry = x + y;
+            // what happenns behind the scene is this
+            string myFirstTry = x.ToString() + y;
+            // -----------------------------------------------
+
+
+
+
+            // int mySecondTry = x + y;.
+            int mySecondTry = x + int.Parse(y);
+
+
+            // .Net allows to implicitly combine different data types together
+            Console.WriteLine(myFirstTry);
 
             Console.ReadLine();
 
